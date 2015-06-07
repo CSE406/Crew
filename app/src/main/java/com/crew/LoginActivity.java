7823627ef532a846c25e2533c097d62b4e844818
Hyeon-Seob Seo <hsseo417@gmail.com>
2015-06-07 13:29:31 +0900

    login activity issue

--- app/src/main/java/com/crew/LoginActivity.java
+++ app/src/main/java/com/crew/LoginActivity.java
@@ -3,23 +3,29 @@ package com.crew;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.crew.ui.MainActivity;
-import com.crew.ui.group.MainFragment;
-import com.crew.ui.material.Button;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
 
 public class LoginActivity extends FragmentActivity {
-    private MainFragment mainFragment;
     private LoginButton loginButton;
     private Button guestButton;
     private CallbackManager callbackManager;
@@ -27,25 +33,39 @@ public class LoginActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
-
         FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+
         callbackManager = CallbackManager.Factory.create();
         loginButton = (LoginButton) findViewById(R.id.facebook_login_button);
+        loginButton.setReadPermissions(Arrays.asList("email"));
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
-
+                        Log.i("facebook","login success");
+                        GraphRequest.newMeRequest(
+                                loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(JSONObject me, GraphResponse response) {
+                                        if (response.getError() != null) {
+                                            Log.e("facebook","handle error");
+                                        } else {
+                                            Toast.makeText(getApplicationContext(), "Welcome " + me.optString("name") + "!", Toast.LENGTH_LONG);
+                                            //String email = me.optString("email");
+                                            //String id = me.optString("id");
+                                        }
+                                    }
+                                }).executeAsync();
                     }
 
                     @Override
                     public void onCancel() {
-
+                        Toast.makeText(getApplicationContext(), "Login Cancel", Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
                     public void onError(FacebookException e) {
-
+                        Log.e("facebook",e.getMessage());
                     }
                 }
         );
